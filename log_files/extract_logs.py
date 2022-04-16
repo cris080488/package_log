6227c0cff937dc04a0980b4b69be84564e61a3f9
criosilva <crisosilva@gmail.com>
2022-04-15 21:47:53 -0300

    modified function variable names

--- log_files/extract_logs.py
+++ log_files/extract_logs.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime, timedelta, time
 
-def send_ping(ip:str, tempo:int, contagem:int) -> str:
+def send_ping(ip:str, interval:int, count:int) -> str:
     '''
 
     :param ip:
@@ -9,8 +9,8 @@ def send_ping(ip:str, tempo:int, contagem:int) -> str:
     :param contagem:
     :return: name of saved file
     '''
-    pings = os.system('ping -i {} -c {} -n {} > ping_{}.txt'.format(tempo,
-                                                                    contagem,
+    pings = os.system('ping -i {} -c {} -n {} > ping_{}.txt'.format(interval,
+                                                                    count,
                                                                     ip,
                                                                     datetime.now().strftime('%d-%m-%Y_%H:%M')))
 
